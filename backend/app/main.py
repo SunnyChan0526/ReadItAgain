8a3dd0330f76e0c4ab32f5a19b730b78acf80cc7
SunnyChan0526 <t110590032@ntut.org.tw>
2023-12-17 21:18:57 +0800

    update upload avatar

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import init_db, get_session, Book, Picture_List, Shopping_Cart, Cart_List, Member, Seller, Customer, Address_List
 from app.models import BookSearch, BookDetail, ShoppingCartList, Token, Profile, Address, AddressCreate, AddressEdit
 from .config import settings
-import shutil
+import shutil, os
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -366,15 +366,19 @@ async def upload_avatar(
     session: AsyncSession = Depends(get_session)
 ):
     user = await get_current_user_data(token, session)
-    # todo: delete origin picture in the img path
-    if avatar.content_type.startswith('image'):
+    
+    if avatar.content_type in ("image/jpg", "image/jpeg", "image/png"):
         img_type = avatar.content_type.split('/')[1]
         file_location = f"./img/avatar/{user.memberaccount}.{img_type}"
         with open(file_location, "wb") as file_object:
             shutil.copyfileobj(avatar.file, file_object)
 
         # 更新用戶的 ProfilePicture
-        user.profilepicture = user.memberaccount + '.'+ img_type
+        if user.profilepicture != 'default.jpg':
+            old_file_location = f"./img/avatar/{user.profilepicture}"
+            if os.path.exists(old_file_location):
+                os.remove(old_file_location)
+        user.profilepicture = f"{user.memberaccount}.{img_type}"
         await session.commit()
     else:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="avatar should be an image")
