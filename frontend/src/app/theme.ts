133c0ea61ff06c136d44c045dda4ea04ce72438c
aidenowo <as9135566@gmail.com>
2023-12-24 19:43:05 +0800

    frontend-bata:Add homepage

---
+++ frontend/src/app/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const riatheme =  createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#c8ab83',
+    },
+    secondary: {
+      main: '#813405',
+    },
+  },
+});
+
+export default riatheme;
